110414b3c1cb428b1380ff082b886805ff384c7a
calculator <122430544+Ravichouhan12@users.noreply.github.com>
2023-10-25 09:33:19 +0530

    Add files via upload

---
+++ Image.js
@@ -0,0 +1,9 @@
+export const localimage = {
+    back:require("../Images/back.jpg.png"),
+    male:require("../Images/male.webp"),
+    male2:require("../Images/male2.jpg"),
+    male3:require("../Images/male3.jpg"),
+    male4:require("../Images/male4.jpg"),
+    male5:require("../Images/male5.jpg"),
+    male6:require("../Images/male6.jpg"),
+}
