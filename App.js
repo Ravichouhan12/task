a2b892366d1ad9b0b3e722fa50f5e45ad843e1a3
calculator <122430544+Ravichouhan12@users.noreply.github.com>
2023-10-25 09:28:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Font } from './Component/Fontt'
+import Navigationss from './Component/Navigations/Navigationss'
+
+const App = () => {
+  return (
+    <View>
+      <Navigationss/>
+    </View>
+  )
+}
+
+export default App
